2f872e229923e386fc55234bb6900e35f37a904c
unknown <C:\Users\vv\AppData\Roaming\The Bat!>
2019-09-29 23:19:31 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const http = require('http');
+
+const PORT = 81;
+
+
+
+const server = http.createServer((req ,res) => {
+    const url = req.url;
+    if (url === "/"){
+        //console.log("/ - works")
+        res.setHeader("Content-type", "text/html");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>Hello World</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
+    }
+    else if (url === "/about") {
+        res.setHeader("Content-type", "text/html");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>About us</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
+    }
+    else{
+        res.setHeader("Content-type", "text/html");
+        //res.setHeader("Content-type", "application/json");
+        res.write("<html>");
+        res.write("<head>");
+        res.write("<title>Web Server</title>");
+        res.write("</head>");
+        res.write("<body>");
+        res.write("<main>");
+        res.write("<h1>404 Page not found!</h1>");
+        res.write("</main>");
+        res.write("</body>");
+        res.write("</html>");
+        res.end();
+    }
+})
+
+
+server.listen(PORT);
+
+console.log(http.STATUS_CODES);
+console.log(http.METHODS);
